58bae1f91f74d0e525b6dc54a3b2ca0d3faa5f39
wangweichao <18211090765@163.com>
2019-12-03 11:49:59 +0800

    添加三方库

--- WWLearnDemo/Pods/Target Support Files/Pods-WWLearnDemo/Pods-WWLearnDemo-frameworks.sh
+++ WWLearnDemo/Pods/Target Support Files/Pods-WWLearnDemo/Pods-WWLearnDemo-frameworks.sh
@@ -163,11 +163,17 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YTKNetwork/YTKNetwork.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YTKNetwork/YTKNetwork.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- WWLearnDemo/WWLearnDemo/Base/AppDelegate.m
+++ WWLearnDemo/WWLearnDemo/Base/AppDelegate.m
@@ -9,8 +9,11 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 #import "YTKNetwork.h"
+#import "UIImageView+WebCache.h"
 
 @interface AppDelegate ()
+
+@property(nonatomic, strong) UIView *launchView;
 
 @end
 
@@ -24,12 +27,43 @@
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    
+    [self initLaunchScreen];
+    
+    
     ViewController *vc = [[ViewController alloc] init];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:vc];
     [self.window makeKeyAndVisible];
     
     return YES;
 }
+
+
+- (void)initLaunchScreen{
+    
+    UIViewController *launchVC = [[UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil] instantiateViewControllerWithIdentifier:@"LaunchScreen"];
+    _launchView = launchVC.view;
+    
+    UIWindow *mainWindow = [UIApplication sharedApplication].keyWindow;
+    _launchView.frame = [UIApplication sharedApplication].keyWindow.frame;
+    [mainWindow addSubview:_launchView];
+    UIImageView *imageView=[[UIImageView alloc]init];
+    [imageView sd_setImageWithURL:[NSURL URLWithString:@"http://pic15.nipic.com/20110628/1369025_192645024000_2.jpg"]];
+        //根据需求添加约束最好
+    imageView.frame = CGRectMake(0, 0, 200, 200);
+    imageView.backgroundColor=[UIColor whiteColor];
+    [_launchView addSubview:imageView];
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(removeLaunchImage) userInfo:nil repeats:NO];
+}
+
+-(void)removeLaunchImage{
+    [_launchView removeFromSuperview];
+    //设置rootViewController的代码
+
+}
+
+
 
 //在应用进入前台或者后台的时候可以重新进行连接或者断开连接
 //进入前台
